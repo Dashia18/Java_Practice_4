ec82e6aabf937a82e7e9d813edd8320475e9dfa8
Dashia <das-bobkova@yandex.ru>
2016-11-02 01:09:16 +0300

    Class Point2D - began

---
+++ src/Point2D/Point2D.java
@@ -0,0 +1,49 @@
+package Point2D;
+
+/**
+ * Created by Daria Serebryakova on 01.11.2016.
+ */
+public class Point2D {
+    public Point2D() {
+    }
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public float[] getXY() {
+        float[] coofr = {x,y};
+        return coofr;
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D " +
+                "(" + x +
+                ", " + y +
+                ")";
+    }
+
+    //data
+    private float x = 0.0f;
+    private float y = 0.0f;
+}
---
+++ src/Point2D/Runner.java
@@ -0,0 +1,14 @@
+package Point2D;
+
+
+
+/**
+ * Created by Daria Serebryakova on 02.11.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        Point2D point1 = new Point2D(1, 2);
+        System.out.println(point1);
+
+    }
+}
